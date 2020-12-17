3427fd4507318bf8a817dc00620f74e584d212be
Shakespearen <alokin123321@gmail.com>
2020-12-17 15:39:38 +0100

    End of section 13

--- API/Startup.cs
+++ API/Startup.cs
@@ -9,6 +9,7 @@ using API.Helpers;
 using AutoMapper;
 using API.Middleware;
 using API.Extensions;
+using StackExchange.Redis;
 
 namespace API
 {
@@ -29,6 +30,13 @@ namespace API
             services.AddControllers();
             services.AddDbContext<StoreContext>(x =>
              x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            services.AddSingleton<IConnectionMultiplexer>(c =>
+            {
+                var configuration = ConfigurationOptions.Parse(_config.GetConnectionString("Redis"),
+                true);
+                return ConnectionMultiplexer.Connect(configuration);
+            });
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
             services.AddCors(opt =>
@@ -60,7 +68,7 @@ namespace API
             app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
-   
+
             app.UseSwaggerDocumentation();
 
             app.UseEndpoints(endpoints =>
